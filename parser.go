d345ea2644c876fc92a3782da7b17cc4597dc923
bfoersterling <bjoern.foersterling@gmail.com>
2025-08-15 09:51:00 +0200

    implemented function get_heading_numberings

--- parser.go
+++ parser.go
@@ -8,26 +8,56 @@ import (
 	"strings"
 )
 
+// get raw heading numbering
+// i.e. [0 1 0 2 0 0]
+// this output could then be pretty printed by omitting zeros
+func get_heading_numberings(current_level int, current_level_count [6]int) (level_count [6]int) {
+	level_count = current_level_count
+
+	level_count[current_level-1]++
+
+	return
+}
+
 func parse_headings(reader io.Reader) []heading {
 	md_scanner := bufio.NewScanner(reader)
 	var headings []heading
 	is_codeblock := false
-	index := 1
+	index := 0
+	//	var hnumbering [6]int
+	// hnum_cursor may have values 0-6
+	//	var hnum_cursor int
 
 	md_scanner.Split(bufio.ScanLines)
 
 	for md_scanner.Scan() {
+		index++
+
 		md_line := md_scanner.Text()
 
 		if strings.HasPrefix(md_line, "```") {
 			is_codeblock = !is_codeblock
 		}
 
-		if !is_codeblock && is_heading(md_line) {
-			headings = append(
-				headings, heading{md_line, get_heading_level(md_line), (index)})
+		if is_codeblock {
+			continue
 		}
-		index++
+
+		if !is_heading(md_line) {
+			continue
+		}
+
+		hlevel := get_heading_level(md_line)
+
+		/*
+			if len(headings) > 0 && hlevel < headings[index-1].level {
+				hnum_cursor++
+			} else if
+			}
+		*/
+
+		headings = append(
+			headings, heading{md_line, hlevel, index})
 	}
 
 	return headings
--- parser_test.go
+++ parser_test.go
@@ -5,6 +5,33 @@ import (
 	"testing"
 )
 
+func Test_get_heading_numbering(t *testing.T) {
+	// 1
+	current_level := 1
+	current_lc := [6]int{0, 0, 0, 0, 0, 0}
+
+	expected_result := [6]int{1, 0, 0, 0, 0, 0}
+
+	test_lc := get_heading_numberings(current_level, current_lc)
+
+	if test_lc != expected_result {
+		t.Fatalf("test_lc and expected_result differ!\n"+
+			"test_lc:\n%v\nexpected_result:\n%v\n", test_lc, expected_result)
+	}
+
+	// 2
+	current_level = 6
+	current_lc = [6]int{1, 2, 0, 8, 0, 1}
+	expected_result = [6]int{1, 2, 0, 8, 0, 2}
+
+	test_lc = get_heading_numberings(current_level, current_lc)
+
+	if test_lc != expected_result {
+		t.Fatalf("test_lc and expected_result differ!\n"+
+			"test_lc:\n%v\nexpected_result:\n%v\n", test_lc, expected_result)
+	}
+}
+
 func Test_parse_headings(t *testing.T) {
 	// 1
 	reader := strings.NewReader("## first section\n\n" +
