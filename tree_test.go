4a76c50c620d0fe0c6faec5b9e8734d83f9fc202
bfoersterling <bjoern.foersterling@gmail.com>
2025-08-29 18:32:50 +0200

    added benchmark of print_tree() and added another unit test for print_tree to replace primitive tests in recipe

--- tree_test.go
+++ tree_test.go
@@ -49,4 +49,45 @@ func Test_print_tree(t *testing.T) {
 			"test_buffer.String():\n%q\nexpected_result:\n%q\n",
 			test_buffer.String(), expected_result)
 	}
+
+	// 3 - big file audio.md
+	test_buffer.Reset()
+
+	expected_result = "audio.md\n" +
+		"|-- 1. ## alsa (1)\n" +
+		"|   |-- 1.1. #### aplay (8)\n" +
+		"|   |-- 1.2. #### configuration (59)\n" +
+		"|   |-- 1.3. #### select default soundcard and/or pcm device (85)\n" +
+		"|   `-- 1.4. #### audioservers (109)\n" +
+		"|-- 2. ## how to figure out if you are using pulseaudio or pipewire? (114)\n" +
+		"|-- 3. ## pipewire (128)\n" +
+		"|   |-- 3.1. #### pipewire - record audio (137)\n" +
+		"|   `-- 3.2. #### pipe audio to pipewire (144)\n" +
+		"|-- 4. ## pulseaudio (151)\n" +
+		"|   |-- 4.1. #### installation (153)\n" +
+		"|   |-- 4.2. #### pulseaudio commands (161)\n" +
+		"|   |-- 4.3. #### testing pulseaudio (171)\n" +
+		"|   |-- 4.4. #### ffmpeg (188)\n" +
+		"|   |-- 4.5. #### play sounds on the command line (204)\n" +
+		"|   `-- 4.6. #### test microphone (219)\n" +
+		"|-- 5. ## Volume problem in movies (239)\n" +
+		"|   |-- 5.1. #### audacity (312)\n" +
+		"|   `-- 5.2. #### location of sound files that applications use (326)\n" +
+		"`-- 6. ## troubleshooting (346)\n" +
+		"    |-- 6.1. #### no audio input from headset (348)\n" +
+		"    `-- 6.2. #### no sound issue on Ubuntu (359)\n"
+
+	print_tree("test_files/audio.md", test_buffer)
+
+	if test_buffer.String() != expected_result {
+		t.Fatalf("test_buffer.String() and expected result differ!\n"+
+			"test_buffer.String():\n%q\nexpected_result:\n%q\n",
+			test_buffer.String(), expected_result)
+	}
+}
+
+func Benchmark_print_tree(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		print_tree("test_files/audio.md", bytes.NewBuffer([]byte("")))
+	}
 }
