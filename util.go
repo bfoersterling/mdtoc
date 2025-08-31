c1665926314e9985b68dd1ce8e15319b4d2456d9
bfoersterling <bjoern.foersterling@gmail.com>
2025-08-31 18:57:59 +0200

    colored output can now be disabled

--- util.go
+++ util.go
@@ -1,9 +1,15 @@
 package main
 
 func start_color_green(color_config string) string {
+	if color_config == "off" {
+		return ""
+	}
 	return "\033[0;32m"
 }
 
 func end_color(color_config string) string {
+	if color_config == "off" {
+		return ""
+	}
 	return "\033[0m"
 }
