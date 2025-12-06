5c605916dd5ab2ffa3124db8374612bd1c8f9eaa
bfoersterling <bjoern.foersterling@gmail.com>
2025-12-06 19:31:44 +0100

    use readline library for interactive mode - support for keyboard shortcuts, history, and autocompletion

--- interactive_mode.go
+++ interactive_mode.go
@@ -1,36 +1,53 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"os"
+	"strings"
 	"unicode"
+
+	"github.com/chzyer/readline"
 )
 
 func do_interactive(args cli_args) {
-	var input string
 	var err error
+
+	var completer = readline.NewPrefixCompleter(
+		readline.PcItem("summary"),
+	)
 
 	print_tree(args.files[0], os.Stdout)
 
-	for {
-		fmt.Printf("mdtoc> ")
+	rli, err := readline.NewEx(&readline.Config{
+		Prompt:          "mdtoc> ",
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+		AutoComplete:    completer,
+	})
 
-		_, err = fmt.Scanf("%s", &input)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		line, err := rli.Readline()
 
 		if err == io.EOF {
-			fmt.Printf("\n")
 			break
 		}
 
-		if input == "" {
+		trimmed_line := strings.TrimSpace(line)
+
+		if trimmed_line == "" {
 			continue
 		}
 
-		if unicode.IsDigit(rune(input[0])) {
-			print_chapter(args.files[0], input, args.color, os.Stdout)
+		if unicode.IsDigit(rune(trimmed_line[0])) {
+			print_chapter(args.files[0], trimmed_line, args.color, os.Stdout)
 		}
 
-		input = ""
+		if (trimmed_line == "s") || (trimmed_line == "summary") {
+			print_tree(args.files[0], os.Stdout)
+		}
 	}
 }
