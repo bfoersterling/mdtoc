2868ae4f565b45f9e04d6c28b895722db00722cd
bfoersterling <bjoern.foersterling@gmail.com>
2025-12-11 18:16:07 +0100

    added help command to interactive mode

--- interactive_mode.go
+++ interactive_mode.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"strings"
@@ -15,6 +16,7 @@ func do_interactive(args cli_args) {
 
 	var completer = readline.NewPrefixCompleter(
 		readline.PcItem("edit"),
+		readline.PcItem("help"),
 		readline.PcItem("summary"),
 	)
 
@@ -50,10 +52,20 @@ func do_interactive(args cli_args) {
 		case unicode.IsDigit(rune(trimmed_line[0])):
 			print_chapter(args.files[0], trimmed_line, args.color, os.Stdout)
 			last_chapter = trimmed_line
-		case (trimmed_line == "s") || (trimmed_line == "summary"):
-			print_tree(args.files[0], os.Stdout)
 		case (trimmed_line == "e") || (trimmed_line == "edit"):
 			edit_chapter(args.files[0], last_chapter)
+		case (trimmed_line == "s") || (trimmed_line == "summary"):
+			print_tree(args.files[0], os.Stdout)
+		case (trimmed_line == "h") || (trimmed_line == "help"):
+			help_imode()
 		}
 	}
+}
+
+func help_imode() {
+	fmt.Printf("List of available commands:\n")
+	fmt.Printf("[0-9]...(heading numbering)\tprint chapter (i.e. type \"2.1.3\")\n")
+	fmt.Printf("e[edit]\t\t\t\tedit chapter that was printed most recently\n")
+	fmt.Printf("h[help]\t\t\t\tprint this help\n")
+	fmt.Printf("s[ummary]\t\t\tprint toc of markdown file\n")
 }
