1565a2c865eb36f31fd6fe190f14d724e11d18b2
bfoersterling <bjoern.foersterling@gmail.com>
2025-12-31 16:05:46 +0100

    implemented func generate_html

--- html.go
+++ html.go
@@ -6,5 +6,27 @@ import (
 )
 
 func print_html(file_path string, writer io.Writer) {
-	fmt.Fprintf(writer, "<p>Not implemented yet.</p>\n")
+	lines := fetch_lines(file_path, "off")
+
+	html := generate_html(lines)
+
+	fmt.Fprintf(writer, html)
+}
+
+func generate_html(lines []line) (html string) {
+	for _, line := range lines {
+		switch v := line.(type) {
+		case heading:
+			html += "<h" + string(v.level) + ">" + v.text + "</h" +
+				string(v.level)
+		case codeline:
+			html += "<code>" + v.text + "</code>"
+		case nonheading:
+			html += "<p>" + v.text + "</p>"
+		}
+
+		html += "\n"
+	}
+
+	return
 }
