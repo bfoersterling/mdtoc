84e376e5d7d4bbaab1b7f1b6725e47618cfd945a
bfoersterling <bjoern.foersterling@gmail.com>
2025-12-31 21:58:18 +0100

    use byte slice as output and bytes.Buffer for building the buffer more efficiently

--- html.go
+++ html.go
@@ -18,27 +18,39 @@ func print_html(files []string, writer io.Writer) (err error) {
 
 	html := generate_html(reader)
 
-	fmt.Fprintf(writer, html)
+	fmt.Fprintf(writer, string(html))
 
 	return
 }
 
-func generate_html(reader io.Reader) (html string) {
+func generate_html(reader io.Reader) (html []byte) {
+	var html_buffer bytes.Buffer
 	lines := parse_lines(reader, "off")
 
 	for _, line := range lines {
 		switch v := line.(type) {
 		case heading:
-			html += "<h" + strconv.Itoa(v.level) + ">" + v.text + "</h" +
-				strconv.Itoa(v.level) + ">"
+			html_buffer.WriteString("<h")
+			html_buffer.WriteString(strconv.Itoa(v.level))
+			html_buffer.WriteString(">")
+			html_buffer.WriteString(v.text)
+			html_buffer.WriteString("</h")
+			html_buffer.WriteString(strconv.Itoa(v.level))
+			html_buffer.WriteString(">")
 		case codeline:
-			html += "<code>" + v.text + "</code>"
+			html_buffer.WriteString("<code>")
+			html_buffer.WriteString(v.text)
+			html_buffer.WriteString("</code>")
 		case nonheading:
-			html += "<p>" + v.text + "</p>"
+			html_buffer.WriteString("<p>")
+			html_buffer.WriteString(v.text)
+			html_buffer.WriteString("</p>")
 		}
 
-		html += "\n"
+		html_buffer.WriteString("\n")
 	}
+
+	html = html_buffer.Bytes()
 
 	return
 }
--- html_test.go
+++ html_test.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 )
 
 func Test_generate_html(t *testing.T) {
 	// example 10 from spec.txt
 	input_10 := []byte("#\tFoo\n")
-	html_10 := "<h1>Foo</h1>\n"
+	html_10 := []byte("<h1>Foo</h1>\n")
 
 	reader_10 := bytes.NewReader(input_10)
 	output_10 := generate_html(reader_10)
 
-	fmt.Printf("%s\n", output_10)
-	fmt.Printf("Expected %s\n", html_10)
+	if bytes.Compare(html_10, output_10) != 0 {
+		t.Fatalf("html_10 and output_10 differ.\n"+
+			"html_10:\n%q\noutput_10:%q\n", html_10, output_10)
+	}
 }
