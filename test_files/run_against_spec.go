87762aca245c1d308246f49ba347034289637d68
bfoersterling <bjoern.foersterling@gmail.com>
2026-01-01 12:13:07 +0100

    - running spec tests seems to work - added just recipe for spec tests

--- test_files/run_against_spec.go
+++ test_files/run_against_spec.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 type example struct {
@@ -15,8 +17,43 @@ type example struct {
 	Section   string `json:"section"`
 }
 
+type result struct {
+	ok      int
+	failed  int
+	errored int
+}
+
+func (e *example) run_spec_test(res *result) {
+	cmd := exec.Command("../mdtoc", "-html", "-unsafe")
+
+	cmd.Stdin = strings.NewReader(e.Markdown)
+
+	out, err := cmd.CombinedOutput()
+
+	fmt.Printf("DEBUG: %+v\n", cmd)
+
+	if err != nil {
+		fmt.Printf("Test %d errored.\n", e.Id)
+		fmt.Printf("actual:\n%q\nexpected:\n%q\n", out, e.Html)
+		res.errored++
+		return
+	}
+
+	if string(out) == e.Html {
+		fmt.Printf("Test %d successful.\n", e.Id)
+		res.ok++
+	} else {
+		fmt.Printf("Test %d failed.\n", e.Id)
+		fmt.Printf("input:\n%q\n", e.Markdown)
+		fmt.Printf("actual:\n%q\nexpected:\n%q\n", out, e.Html)
+		res.failed++
+	}
+}
+
 func main() {
 	fmt.Printf("Running mdtoc against cmark spec.\n")
+
+	var res result
 
 	spec_bytes, err := os.ReadFile("cmark_spec.json")
 
@@ -33,6 +70,8 @@ func main() {
 	}
 
 	for _, example := range examples {
-		fmt.Printf("%+v\n", example)
+		example.run_spec_test(&res)
 	}
+
+	fmt.Printf("%+v\n", res)
 }
