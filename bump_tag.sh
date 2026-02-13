3f04220cd61269065163512f72e81ad41a3806c8
bfoersterling <bjoern.foersterling@gmail.com>
2026-02-13 18:58:18 +0100

    added script to bump semver (#17)

---
+++ bump_tag.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+current_tag="$(git describe --tags --abbrev=0)"
+major_version="$(echo "$current_tag" | cut -d'.' -f1 | cut -d'v' -f2)"
+minor_version="$(echo "$current_tag" | cut -d'.' -f2)"
+patch_version="$(echo "$current_tag" | cut -d'.' -f3)"
+
+echo "Current tag: ${current_tag}"
+
+echo "major_version: ${major_version}"
+echo "minor_version: ${minor_version}"
+echo "patch_version: ${patch_version}"
+
+read -rp "Do you want to increment the major, minor or patch version? (major, minor, patch) "
+
+case "$REPLY" in
+major)
+	((major_version++))
+	;;
+minor)
+	((minor_version++))
+	;;
+patch)
+	((patch_version++))
+	;;
+*)
+	echo "Invalid value - pass major/minor/patch."
+	exit 1
+	;;
+esac
+
+new_version="v${major_version}.${minor_version}.${patch_version}"
+
+echo "New version: ${new_version}"
+
+if git tag "$new_version"; then
+	echo "New tag ${new_version} was set."
+	echo "Push it with \"git push --tags\""
+else
+	echo "Failed to set new tag."
+fi
+
+exit 0
