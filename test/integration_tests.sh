7339c3d5d7ab20c7a2e62c018797a518e6f42acb
bfoersterling <bjoern.foersterling@gmail.com>
2026-06-04 12:01:04 +0200

    added integration test for passing two files in toc mode

--- test/integration_tests.sh
+++ test/integration_tests.sh
@@ -62,13 +62,13 @@ printf "Integration tests for mdtoc.\n\n"
 
 printf "Integration tests start.\n"
 
-# 1
+# 1 - piping input into mdtoc without any options
 output_1=$(echo "# foo" | "$MDTOC")
 expected_1="1. foo (1)"
 
 assert_str_eq "1" "$output_1" "$expected_1"
 
-# 2
+# 2 - passing a basic markdown file without any options
 output_2=$("$MDTOC" "${SCRIPT_DIR}/input/basic.md")
 expected_2=$'1. foo (1)\n'
 expected_2+=$'  1.1. sub foo (3)\n'
@@ -77,32 +77,39 @@ expected_2+=$'  2.1. sub bar (12)'
 
 assert_str_eq "2" "$output_2" "$expected_2"
 
-# 3
+# 3 - like test 2 but with another file
 output_3=$("$MDTOC" "${SCRIPT_DIR}/input/citation.md")
 expected_3=$'1. a great quote (1)\n2. and another one (6)'
 
 assert_str_eq "3" "$output_3" "$expected_3"
 
-# 4
-output_4=$("$MDTOC" -c 2.1 "${SCRIPT_DIR}/input/basic.md")
-expected_4=$'#### sub bar\n\nBar.\n\n```\necho "hello"\n```\n\nMore text.'
+# 4 - no options and two files should print the tocs for both files
+output_4=$("$MDTOC" "${SCRIPT_DIR}/input/citation.md" \
+	"${SCRIPT_DIR}/input/no_headings.md")
+expected_4=$'1. a great quote (1)\n2. and another one (6)\n0. preamble (1)'
 
 assert_str_eq "4" "$output_4" "$expected_4"
 
-# 5 - passing a dir should not segfault (exit code 139), but exit with rc 1.
+# 5 - chapter option
+output_5=$("$MDTOC" -c 2.1 "${SCRIPT_DIR}/input/basic.md")
+expected_5=$'#### sub bar\n\nBar.\n\n```\necho "hello"\n```\n\nMore text.'
+
+assert_str_eq "5" "$output_5" "$expected_5"
+
+# 6 - passing a dir should not segfault (exit code 139), but exit with rc 1.
 "$MDTOC" "$(mktemp -d)" &>/dev/null
 
-assert_int_eq "5" "$?" "1"
+assert_int_eq "6" "$?" "1"
 
-# 6 - passing a dir with interactive mode should not segfault (exit code 139)
+# 7 - passing a dir with interactive mode should not segfault (exit code 139)
 "$MDTOC" -i "$(mktemp -d)" &>/dev/null
 
-assert_rc_eq "6" "1"
+assert_rc_eq "7" "1"
 
-# 7 - passing a dir with edit mode should not segfault (exit code 139)
+# 8 - passing a dir with edit mode should not segfault (exit code 139)
 "$MDTOC" -e "1.3" "$(mktemp -d)" &>/dev/null
 
-assert_rc_eq "7" "1"
+assert_rc_eq "8" "1"
 
 printf "Integration tests end.\n"
 
